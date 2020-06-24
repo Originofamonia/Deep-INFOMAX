f4c6c3e578b25fb762f97ee6dde6b9f42aa6ecaf
username <email>
2020-06-24 00:54:49 -0700

    refactored syntax

--- Model.py
+++ Model.py
@@ -1,32 +1,33 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # model definition
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-import torch, torchvision, torch.nn.functional as F
+import torch
+import torch.nn.functional as F
+
 
 class Encoder(torch.nn.Module):
-    def __init__(self, input_shape=(32,32), num_feature=64, out_size=64):
-        super().__init__()
+    def __init__(self, input_shape=(32, 32), num_feature=64, out_size=64):
+        super(Encoder, self).__init__()
 
         assert isinstance(input_shape, tuple), "tuple of integers."
         self.input_shape = input_shape
-        self.M_shape = (input_shape[0]-3*2, input_shape[1]-3*2)
-        self.M_channels = num_feature*2
+        self.M_shape = (input_shape[0] - 3 * 2, input_shape[1] - 3 * 2)
+        self.M_channels = num_feature * 2
 
         self.c0 = torch.nn.Conv2d(3, num_feature, kernel_size=4, stride=1)
-        self.c1 = torch.nn.Conv2d(num_feature, num_feature*2, kernel_size=4, stride=1)
-        self.c2 = torch.nn.Conv2d(num_feature*2, num_feature*4, kernel_size=4, stride=1)
-        self.c3 = torch.nn.Conv2d(num_feature*4, num_feature*8, kernel_size=4, stride=1)
+        self.c1 = torch.nn.Conv2d(num_feature, num_feature * 2, kernel_size=4, stride=1)
+        self.c2 = torch.nn.Conv2d(num_feature * 2, num_feature * 4, kernel_size=4, stride=1)
+        self.c3 = torch.nn.Conv2d(num_feature * 4, num_feature * 8, kernel_size=4, stride=1)
 
-        in_feature = num_feature*8 * (input_shape[0]-3*4) * (input_shape[1]-3*4)
+        in_feature = num_feature * 8 * (input_shape[0] - 3 * 4) * (input_shape[1] - 3 * 4)
         self.l1 = torch.nn.Linear(in_feature, out_size)
 
-        self.b1 = torch.nn.BatchNorm2d(num_feature*2)
-        self.b2 = torch.nn.BatchNorm2d(num_feature*4)
-        self.b3 = torch.nn.BatchNorm2d(num_feature*8)
+        self.b1 = torch.nn.BatchNorm2d(num_feature * 2)
+        self.b2 = torch.nn.BatchNorm2d(num_feature * 4)
+        self.b3 = torch.nn.BatchNorm2d(num_feature * 8)
 
     def forward(self, x):
-
         E = F.relu(self.c0(x))
         M = F.relu(self.b1(self.c1(E)))
         E = F.relu(self.b2(self.c2(M)))
@@ -38,29 +39,32 @@ class Encoder(torch.nn.Module):
         # M is the M x M feature maps
         return E, M
 
+
 class GlobalDiscriminator(torch.nn.Module):
     r"""
     input of GlobalDiscriminator is the `M` in Encoder.forward, so with
     channels : num_feature * 2, in_channels
     shape    : (input_shape[0]-3*2, input_shape[1]-3*2), M_shape
     """
-    def __init__(self, M_channels, M_shape, E_size, interm_size=512):
-        super().__init__()
 
-        in_channels = M_channels; out_channels = in_channels // 2
+    def __init__(self, M_channels, M_shape, E_size, interm_size=512):
+        super(GlobalDiscriminator, self).__init__()
+
+        in_channels = M_channels
+        out_channels = in_channels // 2
         self.c0 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3)
-        in_channels = out_channels; out_channels = in_channels // 2
+        in_channels = out_channels
+        out_channels = in_channels // 2
         self.c1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3)
 
         # see appendix 1A of https://arxiv.org/pdf/1808.06670.pdf
         # input of self.l0 is the concatenate of E and flattened output of self.c1 (C)
-        in_feature = out_channels * (M_shape[0]-2*2) * (M_shape[1]-2*2) + E_size
+        in_feature = out_channels * (M_shape[0] - 2 * 2) * (M_shape[1] - 2 * 2) + E_size
         self.l0 = torch.nn.Linear(in_feature, interm_size)
         self.l1 = torch.nn.Linear(interm_size, interm_size)
         self.l2 = torch.nn.Linear(interm_size, 1)
 
     def forward(self, E, M):
-
         C = F.relu(self.c0(M))
         C = self.c1(C)
         C = C.view(E.shape[0], -1)
@@ -83,8 +87,9 @@ class LocalDiscriminator(torch.nn.Module):
 
     replicated means that all pixels are the same, they are just copies.
     """
+
     def __init__(self, M_channels, E_size, interm_channels=512):
-        super().__init__()
+        super(LocalDiscriminator, self).__init__()
 
         in_channels = E_size + M_channels
         self.c0 = torch.nn.Conv2d(in_channels, interm_channels, kernel_size=1)
@@ -92,12 +97,12 @@ class LocalDiscriminator(torch.nn.Module):
         self.c2 = torch.nn.Conv2d(interm_channels, 1, kernel_size=1)
 
     def forward(self, x):
-
         score = F.relu(self.c0(x))
         score = F.relu(self.c1(score))
         score = self.c2(score)
 
         return score
+
 
 class PriorDiscriminator(torch.nn.Module):
     r"""
@@ -108,8 +113,9 @@ class PriorDiscriminator(torch.nn.Module):
     This discriminator is trained to distinguish Real and Fake inputs.
     So the Encoder is trained to "fool" this discriminator. (idea of GAN)
     """
-    def __init__(self, E_size, interm_size=(1000,200)):
-        super().__init__()
+
+    def __init__(self, E_size, interm_size=(1000, 200)):
+        super(PriorDiscriminator, self).__init__()
         assert isinstance(interm_size, tuple), "tuple of integers."
 
         self.l0 = torch.nn.Linear(E_size, interm_size[0])
@@ -117,17 +123,17 @@ class PriorDiscriminator(torch.nn.Module):
         self.l2 = torch.nn.Linear(interm_size[1], 1)
 
     def forward(self, x):
-
         score = F.relu(self.l0(x))
         score = F.relu(self.l1(score))
         score = torch.sigmoid(self.l2(score))
 
         return score
 
+
 class DeepInfoMaxLoss(torch.nn.Module):
 
     def __init__(self, alpha=0.5, beta=1.0, gamma=0.1):
-        super().__init__()
+        super(DeepInfoMaxLoss, self).__init__()
 
         self.alpha = alpha
         self.beta = beta
@@ -135,11 +141,13 @@ class DeepInfoMaxLoss(torch.nn.Module):
 
         self.get_models()
 
-    def get_models(self, input_shape=(32,32), num_feature=64, out_size=64, interm_size_G=512, interm_channels_L=512, interm_size_P=(1000,200) ):
-
+    def get_models(self, input_shape=(32, 32), num_feature=64, out_size=64, interm_size_G=512, interm_channels_L=512,
+                   interm_size_P=(1000, 200)):
         self.encoder = Encoder(input_shape=input_shape, num_feature=num_feature, out_size=out_size)
-        self.global_D = GlobalDiscriminator(M_channels=self.encoder.M_channels, M_shape=self.encoder.M_shape, E_size=out_size, interm_size=interm_size_G)
-        self.local_D = LocalDiscriminator(M_channels=self.encoder.M_channels, E_size=out_size, interm_channels=interm_channels_L)
+        self.global_D = GlobalDiscriminator(M_channels=self.encoder.M_channels, M_shape=self.encoder.M_shape,
+                                            E_size=out_size, interm_size=interm_size_G)
+        self.local_D = LocalDiscriminator(M_channels=self.encoder.M_channels, E_size=out_size,
+                                          interm_channels=interm_channels_L)
         self.prior_D = PriorDiscriminator(E_size=out_size, interm_size=interm_size_P)
 
     def forward(self, Y, M, M_fake):
@@ -161,7 +169,7 @@ class DeepInfoMaxLoss(torch.nn.Module):
         # 1st term in equation (8) in https://arxiv.org/pdf/1808.06670.pdf
         Ej = -F.softplus(-self.global_D(Y, M)).mean()
         Em = -F.softplus(-self.global_D(Y, M_fake)).mean()
-        global_loss= (Em - Ej) * self.alpha
+        global_loss = (Em - Ej) * self.alpha
 
         # prior loss
         # 3rd term in equation (8) in https://arxiv.org/pdf/1808.06670.pdf
--- Train.py
+++ Train.py
@@ -1,36 +1,37 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # training script
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-import torch, torchvision, torch.nn.functional as F
-import argparse
-from tqdm import tqdm
 from pathlib import Path
+
+import torch
+import torchvision
+from tqdm import tqdm
 
 from Model import DeepInfoMaxLoss
 
-if __name__ == "__main__":
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = 256
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    batch_size = 20
     num_epochs = 1000
     num_workers = 4
     save_interval = 100
-    version = "cifar10_v2"
     lr = 1e-4
 
     # image size (3,32,32)
     # batch size must be an even number
     # shuffle must be True
-    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),])
-    train_dataset = torchvision.datasets.cifar.CIFAR10("~/.torch/", download=True, transform=transform)
-    train_loader  = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=num_workers)
+    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), ])
+    train_dataset = torchvision.datasets.cifar.CIFAR10("data", download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True,
+                                               num_workers=num_workers)
 
     dim = DeepInfoMaxLoss(alpha=0.5, beta=1.0, gamma=0.1).to(device)
     optimizer = torch.optim.Adam(dim.parameters(), lr=lr)
 
     dim.train()
-    for epoch in range(1, num_epochs+1):
+    for epoch in range(1, num_epochs + 1):
         Batch = tqdm(train_loader, total=len(train_dataset) // batch_size)
         for i, (data, target) in enumerate(Batch, 1):
             data = data.to(device)
@@ -39,13 +40,17 @@ if __name__ == "__main__":
             # shuffle batch to pair each element with another
             M_fake = torch.cat((M[1:], M[0].unsqueeze(0)), dim=0)
             loss = dim(Y, M, M_fake)
-            Batch.set_description(f"[{epoch:>3d}/{num_epochs:<3d}]Loss/Train: {loss.item():1.5e}")
+            Batch.set_description("[{epoch:>3d}/{num_epochs:<3d}]Loss/Train: {loss.item():1.5e}")
             dim.zero_grad()
             loss.backward()
             optimizer.step()
 
         # checkpoint and save models
         if epoch % save_interval == 0:
-            file = Path(f"./Models/{version}/checkpoint_epoch_{epoch}.pkl")
+            file = Path("./Models/{version}/checkpoint_epoch_{epoch}.pkl")
             file.parent.mkdir(parents=True, exist_ok=True)
             torch.save(dim.state_dict(), str(file))
+
+
+if __name__ == "__main__":
+    main()
